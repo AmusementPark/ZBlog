5842a2826717b1b22a5696a21d84660fe7d76186
Sheng Hao Zhu <shhaozhu@cn.ibm.com>
2016-05-05 21:09:57 +0800

    Initial commit

---
+++ themes/bootstrap/scripts/zshzcc.js
@@ -0,0 +1,32 @@
+/**
+ * Created by SimonZhu on 16/4/18.
+*/
+//hexo.extend.generator.register('advertisement', function(locals) {
+//    //var config = this.config;
+//    //var posts = locals.posts;
+//    //var paginationDir = config.pagination_dir || 'page';
+//    //
+//    //console.log(posts);
+//    //return pagination('', posts, {
+//    //    perPage: config.index_generator.per_page,
+//    //    layout: ['index', 'archive'],
+//    //    format: paginationDir + '/%d/',
+//    //    data: {
+//    //        __index: true
+//    //    }
+//    //});
+//
+//    //locals.post.data.forEach(function(post){
+//    //    console.log(post);
+//    //})
+//    console.log(locals.posts.data[0]);
+//    return locals.posts;
+//    //return {
+//    //    path: "advertisement",
+//    //    data: function () { // Hexo会 data 返回的数据写入到 path 指定的文件里
+//    //        console.log('@advertisement');
+//    //        return fs.createReadStream('themes/' + hexo.config.theme + 'advertisement/adv1.md'); // 拿模板目录下的 README 文件
+//    //    }
+//    //}
+//});
+//
